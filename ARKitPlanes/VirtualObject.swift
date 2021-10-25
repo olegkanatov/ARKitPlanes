b39e2c5dccb69355707edfe78ece3fba2b7f5a1f
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-25 14:11:08 +0300

    add SCNReferenceNode

---
+++ ARKitPlanes/VirtualObject.swift
@@ -0,0 +1,27 @@
+//
+//  VirtualObject.swift
+//  ARKitPlanes
+//
+//  Created by Oleg Kanatov on 25.10.21.
+//
+
+import SceneKit
+
+class VirtualObject: SCNReferenceNode {
+    
+    static let availableObjects: [SCNReferenceNode] = {
+        
+        guard let modelsURLs = Bundle.main.url(forResource: "art.scnassets", withExtension: nil) else { return [] }
+        
+        let fileEnumirator = FileManager().enumerator(at: modelsURLs, includingPropertiesForKeys: nil)!
+        
+        return fileEnumirator.flatMap{ element in
+            
+            let url = element as! URL
+            
+            guard url.pathExtension == "scn" else { return nil }
+            
+            return VirtualObject(url: url)
+        }
+    }()
+}
