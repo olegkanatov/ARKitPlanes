40682aee931f319838e1f0708d1dfe697d23f9c5
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-25 14:35:09 +0300

    add Placing 3D objects

--- ARKitPlanes/ViewController.swift
+++ ARKitPlanes/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        //        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         sceneView.autoenablesDefaultLighting = true
         
@@ -43,7 +43,14 @@ class ViewController: UIViewController {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(placeBox))
         tapGestureRecognizer.numberOfTapsRequired = 1
         self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(placeVirtualObject))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        self.sceneView.addGestureRecognizer(doubleTapGestureRecognizer)
+        
+        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
     }
+    
     
     @objc func placeBox(tapGesture: UITapGestureRecognizer) {
         
@@ -64,6 +71,29 @@ class ViewController: UIViewController {
         
         let box = Box(atPosition: position)
         sceneView.scene.rootNode.addChildNode(box)
+    }
+    
+    @objc func placeVirtualObject(tapGesture: UITapGestureRecognizer) {
+        
+        let sceneView = tapGesture.view as! ARSCNView
+        let location = tapGesture.location(in: sceneView)
+        
+        let hitTestResult = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        guard let hitResult = hitTestResult.first else { return }
+        
+        createVirtualObject(hitResult: hitResult)
+    }
+    
+    func createVirtualObject(hitResult: ARHitTestResult) {
+        
+        let position = SCNVector3(hitResult.worldTransform.columns.3.x,
+                                  hitResult.worldTransform.columns.3.y,
+                                  hitResult.worldTransform.columns.3.z)
+        
+        let virtualObject = VirtualObject.availableObjects[1]
+        virtualObject.position = position
+        virtualObject.load()
+        sceneView.scene.rootNode.addChildNode(virtualObject)
     }
     
     override func viewWillAppear(_ animated: Bool) {
